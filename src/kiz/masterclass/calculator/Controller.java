ddb58cd0443cf450ff48925348508f602c373ffb
Yel <arielaustria01@gmail.com>
2019-08-29 20:03:15 +0800

    08/29/2019

--- src/kiz/masterclass/calculator/Controller.java
+++ src/kiz/masterclass/calculator/Controller.java
@@ -2,17 +2,86 @@ package kiz.masterclass.calculator;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 
-public class Controller extends Operation {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Controller {
+    @FXML
+    private TextField txt_input;
+
+    private static double total = 0;
+    private static double operandOne = 0;
+    private static double operandTwo = 0;
+    private static String operator = "";
+    private boolean flag = false;
+    private boolean switchOn = false;
+
+
+    private static List<String> series = new ArrayList<>();
 
     @FXML
     public void onButtonClick(ActionEvent event) {
+        series.add(getKey(event));
+        String in = "";
+        for (String next : series) {
+            in = in + next;
+        }
+        txt_input.setText(in);
+        switchOn = false;
+    }
+
+
+    private double getOperatorResult() {
+        double result = 0;
+//        if (operator.equals("+")) {
+//            txt_input.setText((operandOne + operandTwo) + "");
+//            result = (operandOne + operandTwo);
+//        } else
+        switchOn = true;
+        series.clear();
+        System.out.println(result);
+        return result;
+    }
+
+    private void processingOperatorResult() {
+        if (!switchOn) {
+            if (!flag) {
+                operandOne = Double.parseDouble(txt_input.getText());
+                total = operandOne = getOperatorResult();
+                flag = true;
+            } else {
+                operandTwo = Double.parseDouble(txt_input.getText());
+                total = operandTwo = getOperatorResult();
+                flag = false;
+            }
+        }
+    }
+
+    @FXML
+    public void getOperatorWithResult(ActionEvent event) {
+        operator = getKey(event);
+        processingOperatorResult();
+    }
+
+    @FXML
+    public void equal() {
+
+        txt_input.setText((total + Double.parseDouble(txt_input.getText())) + "");
+        //reset
+        total = 0;
+        operandOne = total;
+        operandTwo = total;
+        series.clear();
+    }
+
+
+    @org.jetbrains.annotations.NotNull
+    private static String getKey(ActionEvent event) {
         String key = event.getSource().toString();
         key = key.substring(key.length() - 2, key.length() - 1);
-        System.out.println(key);
-        operateATask(key);
-
+        return key;
     }
 
 }
--- src/module-info.java
+++ src/module-info.java
@@ -3,6 +3,7 @@ module Calculator {
     requires javafx.fxml;
     requires javafx.controls;
     requires graphicx;
+    requires org.jetbrains.annotations;
 
     opens kiz.masterclass.calculator;
 
